ea1b47fe9678a4ba131a780979d97742c9dc6a21
克喵Kemeow <kemiao@kmblog.icu>
2025-03-26 13:13:00 +0800

    Update aplayer.js

--- music/aplayer.js
+++ music/aplayer.js
@@ -1,25 +1,14 @@
-!(function () {
-    var oldLoadAp = window.onload;
-    window.onload = function () {
-        oldLoadAp && oldLoadAp();
-
-        new APlayer({
-            container: document.getElementById('aplayer'),
-            mini: true,
-            autoplay: false,
-            loop: 'all',
-            order: 'list',
-            theme: '#b7daff',
-            preload: 'none',
-            audio: [
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+	autoplay: true, //自动播放
+    audio: [
                 {
             name: '一句话形容不了终极笔记',
             artist: '应有棠 / 御A桑 / 灬阿楚灬 / 绯言 / 小山xl / 霄镁 / 堇墨安歌 / 天罗 / 叶落梦中 / 烫不直的自然卷',
             url: 'http://music.163.com/song/media/outer/url?id=1957502053.mp3',
             cover: 'https://s2.loli.net/2025/03/23/sqe5bQ8pzB9IWmc.jpg',
             lrc: 'https://music-source.kemeow.top/lrc/1.lrc'
-                }
-            ]
-        });
-    }
-})();
+    },
+	]
+});
